d41f3bdf01ff02ee023c70ddb9ca5141fbfef307
Roland Golla <roland.golla@wmdb.de>
2016-02-05 09:27:46 +0100

    add login button mouse over assert

--- tests/acceptance/LoginCept.php
+++ tests/acceptance/LoginCept.php
@@ -3,4 +3,20 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('check login functions');
 $I->amOnPage('/typo3');
 $I->waitForElement('#t3-username');
+
+$I->wantTo('redirect to slash url');
 $I->canSeeCurrentUrlEquals('/typo3/');
+
+$I->wantTo('mouse over css change login button');
+$bc = $I->executeInSelenium(function(\Facebook\WebDriver\Remote\RemoteWebDriver $webdriver) {
+    return $webdriver->findElement(WebDriverBy::cssSelector('#t3-login-submit'))->getCSSValue('background-color');
+});
+
+$I->moveMouseOver('#t3-login-submit');
+$bcmo = $I->executeInSelenium(function(\Facebook\WebDriver\Remote\RemoteWebDriver $webdriver) {
+    return $webdriver->findElement(WebDriverBy::cssSelector('#t3-login-submit'))->getCSSValue('background-color');
+});
+$this->assertFalse($bc == $bcmo);
+
+$I->wantTo('loading hint');
+
